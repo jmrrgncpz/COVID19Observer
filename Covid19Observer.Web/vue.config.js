c7295edaf73fa79f8ee7afe2e77be8ff89ade5ea
jmrrgncpz <paz.jesr@gmail.com>
2021-05-17 20:04:16 +0800

    edit publish dir

--- Covid19Observer.Web/vue.config.js
+++ Covid19Observer.Web/vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     devServer: {
         proxy : 'http://localhost:30193'
-    }
+    },
+    publicPath: process.env.NODE_ENV == 'production' ? 'https://jmrrgncpz.github.io/COVID19Observer' : '/'
 }
