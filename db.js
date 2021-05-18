14507ced8244c5b10df86db10a5314435ecfbf9e
jmrrgncpz <paz.jesr@gmail.com>
2021-05-18 12:38:05 +0000

    Update heroku-express to output generated at 9b3854a

--- db.js
+++ db.js
@@ -2,14 +2,19 @@ var pgp = require('pg-promise')();
 var db = pgp('postgres://postgres:postgres@localhost:5433/COVIDCases');
 
 const getConfirmedCases = (req, res, next) => {
-    db.any('SELECT * FROM "covid_observations" WHERE "ObservationDate" = $1 Limit $2', [req.query.observationDate, req.query.maxCount])
+    db.any(`
+        SELECT "Country" as country,
+            "Confirmed" as confirmed,
+            "Deaths" as deaths,
+            "Recovered" as recovered
+        FROM "covid_observations"
+        WHERE "ObservationDate" = $1
+        ORDER BY "confirmed" DESC
+        LIMIT $2`,
+    [req.query.date, parseInt(req.query.max_results)])
     .then(data => {
         res.status(200)
-            .json({
-                status: 'success',
-                data,
-                message: 'Confirmed cases loaded'
-            })
+            .json({ observations: data })
     }).catch(ex => {
         return next(ex);
     });
--- server.js
+++ server.js
@@ -2,10 +2,11 @@
 const port = process.env.PORT || 3000;
 const express = require('express');
 const db = require('./db.js');
+const cors = require('cors');
 const app = express();
 
 app.get('/api/top/confirmed', db.getConfirmedCases)
-
+app.use(cors());
 app.listen(port, () => {
     console.log(`Listening to http://localhost:${port}`)
 })
